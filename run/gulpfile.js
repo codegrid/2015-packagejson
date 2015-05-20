b9e430a573892df68a29c2772e97bd53f490dfa5
Kyosuke <nakamura@pxgrid.com>
2015-05-20 19:25:48 +0900

    nom run **** で用意したコマンドが実行できるサンプルを追加

---
+++ run/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+ 
+gulp.task('build', function() {
+  console.log('build task!');
+});
+
+gulp.task('deploy', function() {
+  console.log('deploy task!');
+});
+
+gulp.task('pack', function() {
+  console.log('pack task!');
+});
